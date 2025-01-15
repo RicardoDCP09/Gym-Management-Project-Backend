b639ba76a154c18ff8381f4b2b1b89c1f9c55210
Ricardo <154460778+RicardoDCP09@users.noreply.github.com>
2025-01-15 14:08:00 -0400

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,7 +5,7 @@ import { verifyToken } from "../middlewares/jwt.middleware.js"
 
 const router = Router()
 
-router.post('/login', login)
+router.get('/login', login)
 router.post('/register', register)
 router.get('/profile', verifyToken, profile)
 router.post('/logout', logout)
