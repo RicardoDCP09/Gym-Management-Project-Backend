c033ef062f135c9a7af382a22dd39ed38e625286
Ricardo <154460778+RicardoDCP09@users.noreply.github.com>
2025-02-05 00:24:28 -0400

    Update orm.js

--- src/database/orm.js
+++ src/database/orm.js
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 import { DB_DATABASE, DB_USER, DB_PASSWORD } from '../config.js'
 
 const sequelize = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
-    host: 'localhost',
+    host: '127.0.0.1',
     dialect: 'postgres',
     logging: false,
 });
