112e34d51b0d4d7613413621c1ceee3a78555917
Ricardo <154460778+RicardoDCP09@users.noreply.github.com>
2025-02-05 00:27:40 -0400

    Update orm.js

--- src/database/orm.js
+++ src/database/orm.js
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 import { DB_DATABASE, DB_USER, DB_PASSWORD } from '../config.js'
 
 const sequelize = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
-    host: '127.0.0.1',
+    host: 'project-backend-production-2264.up.railway.app',
     dialect: 'postgres',
     logging: false,
 });
